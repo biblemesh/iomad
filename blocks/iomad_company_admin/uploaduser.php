8d4b7a1de68a82457b8974580eecbd74a01832cd
Derick Turner <derick@e-learndesign.co.uk>
2024-11-04 13:14:27 +0000

    IOMAD: bulk upload of users doesn't send enrolment emails

--- blocks/iomad_company_admin/uploaduser.php
+++ blocks/iomad_company_admin/uploaduser.php
@@ -992,6 +992,7 @@ if (!empty($cancelled)) {
 
                     company_user::enrol($user, [$ccache[$shortname]->id], $companyid , $roleid);
                     $coursecontext = context_course::instance($ccache[$shortname]->id);
+                    EmailTemplate::send('user_added_to_course', ['course' => $ccache[$shortname], 'user' => $user]);
 
                     // find group to add to
                     if (!empty($user->{'group'.$i})) {
@@ -1054,9 +1055,13 @@ if (!empty($cancelled)) {
                     // add the user to the courses selected in the upload form.
                     $courseids = array();
                     foreach ($formdata->selectedcourses as $selectedcourse) {
-                        $courseids[] = $selectedcourse->id;
+                        $courseids[] = $selectedcourse;
                     }
                     company_user::enrol($user, $courseids, $companyid);
+                    foreach ($courseids as $courseid) {
+                        $emailcourse = $DB->get_record('course', ['id' => $courseid]);
+                        EmailTemplate::send('user_added_to_course', ['course' => $emailcourse, 'user' => $user]);
+                    }
                 }
                 if (preg_match('/^department\d+$/', $column)) {
                     $i = substr($column, 10);
@@ -1107,7 +1112,11 @@ if (!empty($cancelled)) {
 
             // Enrol user into courses that were selected on the form.
             if (isset($formdata->selectedcourses) ) {
-                company_user::enrol($user, array_keys($formdata->selectedcourses) );
+                company_user::enrol($user, array_values($formdata->selectedcourses) );
+                foreach (array_values($formdata->selectedcourses) as $courseid) {
+                    $emailcourse = $DB->get_record('course', ['id' => $courseid]);
+                    EmailTemplate::send('user_added_to_course', ['course' => $emailcourse, 'user' => $user]);
+                }
             }
 
             // Assign any licenses.
